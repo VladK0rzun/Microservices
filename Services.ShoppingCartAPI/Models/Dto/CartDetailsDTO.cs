b7a94e0020386f6652a1a76b89171fb259fd4cdc
VladK0rzun <vlad2012759@gmail.com>
2023-07-22 10:40:21 +0300

    Add DTO

---
+++ Services.ShoppingCartAPI/Models/Dto/CartDetailsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Services.ShoppingCartAPI.Models.Dto
+{
+    public class CartDetailsDTO
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public CartHeader? CartHeader { get; set; }
+        public int ProductId { get; set; }
+        public ProductDTO? Product { get; set; }
+        public int Count { get; set; }
+    }
+}
